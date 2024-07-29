23d2c78eee10e47f5f872e442df31c228f935832
18107 <18107@users.noreply.github.com>
2024-07-30 08:21:25 +0930

    Added VoidManager support

---
+++ VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace NoUnrepairableDamage
+{
+    internal class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Host;
+
+        public override string Author => "Arpharel";
+
+        public override string Description => "Allows you to repair your ship to full health";
+    }
+}

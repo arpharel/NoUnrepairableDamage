ce885e3e1a430a77e4b599b6ea469dfd27b6861c
Brandon Leger <bleger@dmzsolutions.com>
2025-05-24 21:50:29 -0500

    Added additional check to repair all remaining damage when repairing the last active breach. This does require a breach to repair the ship as there isn't really a mechanism to repair without a breach in game right now.

--- VoidManagerPlugin.cs
+++ VoidManagerPlugin.cs
@@ -6,8 +6,8 @@ namespace NoUnrepairableDamage
     {
         public override MultiplayerType MPType => MultiplayerType.Host;
 
-        public override string Author => "Arpharel";
+        public override string Author => "Arpharel, Miranoff";
 
-        public override string Description => "Allows you to repair your ship to full health";
+        public override string Description => "Allows you to repair your ship to full health. Hull Breach is required to be able to repair to full.";
     }
 }
